a573838fcfdc5922916c6895c336fac85f3f50c5
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-05-26 14:51:32 +0500

    get user, add user to database and fixed auth issues

--- lib/ui/screens/home/home_provider.dart
+++ lib/ui/screens/home/home_provider.dart
@@ -1,8 +1,26 @@
+import 'package:chat_app/core/models/app_user.dart';
 import 'package:chat_app/core/services/auth_services.dart';
+import 'package:chat_app/core/services/database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeProvider extends ChangeNotifier {
   final _auth = AuthServices();
+  final _db = DatabaseService();
+  final fb = FirebaseFirestore.instance;
+  List<AppUser> users = [];
+
+  // HomeProvider() {
+  //   getUser();
+  // }
+
+  // getUser() async {
+  //   users = await _db.getUser();
+  //   if (users.isNotEmpty) {
+  //     print("app user ${users.length}");
+  //   }
+  //   notifyListeners();
+  // }
 
   signOut() async {
     try {
--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,11 +1,15 @@
+import 'package:chat_app/core/models/app_user.dart';
+import 'package:chat_app/main.dart';
 import 'package:chat_app/ui/screens/home/home_provider.dart';
+import 'package:chat_app/ui/widgets/chat_card.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  List<AppUser> users = [];
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +36,34 @@ class HomeScreen extends StatelessWidget {
               child: const Icon(Icons.add_comment_rounded),
             ),
           ),
-          body: Column(
-            children: [],
+          body: StreamBuilder(
+            stream: model.fb.collection("app_user").snapshots(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  final users = snapshot.data!.docs
+                      .map((e) => AppUser.fromJson(e.data(), e.id))
+                      .toList();
+
+                  if (users.isNotEmpty) {
+                    return ListView.builder(
+                        itemCount: users.length,
+                        itemBuilder: (context, index) {
+                          return ChatCard(
+                            user: users[index],
+                          );
+                        });
+                  } else {
+                    return const Center(child: Text("No connections found"));
+                  }
+              }
+            },
           ),
         ),
       ),
